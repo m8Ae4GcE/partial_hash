b8754f30b70909bdf85d4068e06f670d8c15eff4
m8Ae4GcE <91350599+m8Ae4GcE@users.noreply.github.com>
2023-01-20 20:00:05 +0100

    v2.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-[![percent](https://forthebadge.com/images/badges/0-percent-optimized.svg)](https://forthebadge.com) [![love](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![go](https://forthebadge.com/images/badges/made-with-go.svg)](https://forthebadge.com) [![black-magic](https://forthebadge.com/images/badges/powered-by-black-magic.svg)](https://forthebadge.com) [![ask](https://forthebadge.com/images/badges/you-didnt-ask-for-this.svg)](https://forthebadge.com)  [![release](https://img.shields.io/badge/Release-v1.2-green?style=for-the-badge)](https://forthebadge.com)
+[![percent](https://forthebadge.com/images/badges/0-percent-optimized.svg)](https://forthebadge.com) [![love](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![go](https://forthebadge.com/images/badges/made-with-go.svg)](https://forthebadge.com) [![black-magic](https://forthebadge.com/images/badges/powered-by-black-magic.svg)](https://forthebadge.com) [![ask](https://forthebadge.com/images/badges/you-didnt-ask-for-this.svg)](https://forthebadge.com)  [![release](https://img.shields.io/badge/Release-v2.0-green?style=for-the-badge)](https://forthebadge.com)
 
 # partial_hash
-Partial sha256 of file (depending on the chosen buffer) because when you are in a hurry and that is necessary, this tool saves you time.
+Partial md5 of file (depending on the chosen buffer) because when you are in a hurry and that is necessary, this tool saves you time.
 
 # Run
 
@@ -11,36 +11,50 @@ Use the right executable depending on your operating system and processor archit
 
 ```
 USAGE:
-    partial_hash [[-f <file>] or [-d <recursive_folder>]] [-b <buffer_size>]
+    partial_hash [[-f <file>] or [-d <recursive_folder>]] [-b <buffer_size>] [--output csv]
 
 OPTIONS (only -f or -d):
     -f, --file <file>                  File to be hashed.
     -d, --directory <recursive_path>   Folder where files will be recursively hashed.
     -b, --buffer <buffer_size>         Size of the buffer in bytes.
+    --output                           Output format. (Default is stdout)
 ```
 
 For example:
 
 ```
 ./partial_hash -f hello.txt -b 200
-Size of the buffer (in bytes):  200
-hello.txt;887a8f98c79fd7c3b036448d56fa2b6d8b91f4461125621587beeef63b1e4f29;1/1
+Version :  2.0
+Size of the buffer (in bytes) :  200
+
+hello.txt    5dd4a8e994400c28b21f8f42f54f443f
 
 ./partial_hash -d /folder/subfolder/
-Size of the buffer (in bytes):  100000000
-/folder/subfolder/file1;426175749bf6a302057e80109bb9a90fd7c56bda22c73c5a34bbc85197c25c2d;1/2
-/folder/subfolder/hello.txt;887a8f98c79fd7c3b036448d56fa2b6d8b91f4461125621587beeef63b1e4f29;2/2
+Version :  2.0
+Size of the buffer (in bytes) :  100000000
+
+/folder/subfolder/hello.txt       5dd4a8e994400c28b21f8f42f54f443f
+/folder/subfolder/file1       2f282b84e7e608d5852449ed940bfc51
 ```
 
 For Windows, same options but open *partial_hash.exe* with a command interpreter (tested with Powershell and cmd.exe).
 
 > **Warning** The format of file paths on Windows is "C:\\..." if C is the root of the drive to be hashed. 
 
+# Output format (--output)
+
+No argument products :  \<filename>   \<hash>
+
+"--output" is not possible in single file mode.
+
+"--output csv" products : \<filename>;\<hash>;\<boolean if is a partial hash or not>
+
+
 # How it works
 
 A buffer (defined by the argument "-b" and by default 100MB) is used. 
-If the file is smaller than the buffer size, then it will be calculated in sha256.
-If the file is larger than the buffer size, then the tool will take the first X and last X bytes (where X is the buffer) to calculate the sha256. Without any modification of the buffer size, this creates a 200MB hash (the first 100MB and the last 100MB of the file).
+If the file is smaller than the buffer size, then it will be calculated in md5.
+If the file is larger than the buffer size, then the tool will take the first X and last X bytes (where X is the buffer) to calculate the md5. Without any modification of the buffer size, this creates a 200MB hash (the first 100MB and the last 100MB of the file).
 
 # FAQ
 
@@ -48,7 +62,7 @@ If the file is larger than the buffer size, then the tool will take the first X 
 
 Why not...
 
-## Why not a real sha256 of the file? The integrity of this file is questionable with your tool and there is a possibility of hash collision in my lab...
+## Why not a real md5 of the file? The integrity of this file is questionable with your tool and there is a possibility of hash collision in my lab...
 1. The time :
 ```
 $ time ./partial_hash -f 11G_urandom_file 
@@ -58,7 +72,7 @@ real    0m7.674s
 user    0m6.285s
 sys     0m3.945s
 
-$ time sha256sum 11G_urandom_file 
+$ time md5sum 11G_urandom_file 
 53f52c5d14cd467246a4d5bcaabd48ee032af8e679470654e2a0d014ab548e5e  11G_urandom_file
 
 real    6m7.376s
--- partial_hash.go
+++ partial_hash.go
@@ -1,9 +1,7 @@
-//v1.2
-
 package main
 
 import (
-	"crypto/sha256"
+	"crypto/md5"
 	"flag"
 	"fmt"
 	"io"
@@ -12,47 +10,133 @@ import (
 	"os"
 	"runtime"
 	"strings"
+	"time"
 )
+
+// Bar ...
+type Bar struct {
+	percent int64  // progress percentage
+	cur     int64  // current progress
+	total   int64  // total value for progress
+	rate    string // the actual progress bar to be printed
+	graph   string // the fill value for progress bar
+}
 
 // Define some variables to receive command-line values
 var filename string
 var path_to_file string
 var buffer_size uint64
+var export_format string
+
+const YYYYMMDDhhmmss = "2006-01-02_15-04-05"
+
+var export_filename string
+var version = "2.0"
 
 // Array to store the usage order
 var UsageOrder []string
 
+func (bar *Bar) NewOption(start, total int64) {
+	bar.cur = start
+	bar.total = total
+	if bar.graph == "" {
+		bar.graph = "#"
+	}
+	bar.percent = bar.getPercent()
+	for i := 0; i < int(bar.percent); i += 1 {
+		bar.rate += bar.graph // initial progress position
+	}
+}
+
+func (bar *Bar) getPercent() int64 {
+	return int64((float32(bar.cur) / float32(bar.total)) * 50)
+}
+
+func (bar *Bar) Play(cur int64) {
+	bar.cur = cur
+	last := bar.percent
+	bar.percent = bar.getPercent()
+	if bar.percent != last {
+		var i int64 = 0
+		for ; i < bar.percent-last; i++ {
+			bar.rate += bar.graph
+		}
+		fmt.Printf("\r[%-50s]%3d%% %8d/%d", bar.rate, bar.percent*2, bar.cur, bar.total)
+	}
+}
+
+func (bar *Bar) Finish() {
+	fmt.Println()
+}
+
 func main() {
 
 	// -h
-	flag.Usage = Parameters
+	flag.Usage = parameters
 	// Setup flags and parse command line parameters
-	setParameters()
+	set_parameters()
 	flag.Parse()
 
+	now := time.Now().UTC()
+
+	//<YYYY-MM-DD_hh-mm-ss>.partial_hash_<version>.buffer_<buffer_size_in_bytes>.csv
+	export_filename = (now.Format(YYYYMMDDhhmmss)) + ".partial_hash_" + version + ".buffer_" + fmt.Sprint(buffer_size) + "." + export_format
+
 	// Conditions
-	if (path_to_file) != "" && (filename) != "" || len(os.Args) < 2 {
-		Version()
-		Parameters()
+	if (path_to_file != "" && filename != "") || (len(os.Args) < 2) || (path_to_file == "" && filename == "") {
+		parameters()
 		return
 	} else if filename != "" {
-		Version()
-		fmt.Println("Size of the buffer (in bytes): ", buffer_size)
-		calc_hash_file(buffer_size, filename, 1, 1)
+		if export_format == "stdout" {
+			stdout_header()
+		} else {
+			fmt.Print("Output format ", export_format, " does not exist or not possible", "\n\n")
+			parameters()
+			return
+		}
+		calc_hash_file(buffer_size, filename, false)
 
 	} else if path_to_file != "" {
-		Version()
-		fmt.Println("Size of the buffer (in bytes): ", buffer_size)
+		if export_format == "stdout" {
+			stdout_header()
+		} else if export_format == "csv" {
+			create_output_file(export_filename)
+			stdout_header()
+		} else {
+			fmt.Print("Output format ", export_format, " does not exist", "\n\n")
+			parameters()
+			return
+		}
 		recusive(buffer_size, path_to_file)
 	}
+}
+
+func stdout_header() {
+	fmt.Println("Version : ", version)
+	fmt.Println("Size of the buffer (in bytes) : ", buffer_size)
+	fmt.Println("")
+}
+
+func create_output_file(export_filename string) {
+
+	// create the file
+	f, err := os.Create(export_filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// close the file with defer
+	defer f.Close()
+	f.WriteString("filename;hash;is_partial_hash")
 
 }
 
-func Version() {
-	fmt.Println("Version : 1.2")
-}
-
-func Parameters() {
+func parameters() {
+	fmt.Println("Version : ", version)
+	fmt.Println("Size of the buffer (in bytes) : ", buffer_size)
+	fmt.Println("Directory : ", path_to_file)
+	fmt.Println("Filename : ", filename)
+	fmt.Println("Output format : ", export_format)
+	fmt.Println("")
 
 	if len(UsageOrder) == 0 {
 		fmt.Fprintf(os.Stderr, "Error: Attempting to use Parameters, but UsageOrder is not set\n")
@@ -77,15 +161,15 @@ func Parameters() {
 			usageMap[f.Name] = fmt.Sprintf("\t  -%s  \t%s", f.Name, f.Usage)
 		}
 	})
-	fmt.Printf("Usage %s [-f <file>] or [-d <recursive_folder>] [-b <buffer_size>]:\n", os.Args[0])
+	fmt.Printf("Usage %s [[-f <file>] or [-d <recursive_folder>]] [-b <buffer_size>] [--output csv] :\n", os.Args[0])
 	for s := range UsageOrder {
 		fmt.Println(usageMap[UsageOrder[s]])
 	}
 }
 
-func setParameters() {
+func set_parameters() {
 	// Set usage order for display
-	UsageOrder = []string{"file", "f", "directory", "d", "buffer", "b"}
+	UsageOrder = []string{"file", "f", "directory", "d", "buffer", "b", "output"}
 
 	flag.StringVar(&filename, "file", filename, "File to be hashed.")
 	flag.StringVar(&filename, "f", filename, "")
@@ -96,44 +180,57 @@ func setParameters() {
 	flag.Uint64Var(&buffer_size, "buffer", 100000000, "Size of the buffer in bytes.")
 	flag.Uint64Var(&buffer_size, "b", 100000000, "")
 
+	flag.StringVar(&export_format, "output", "stdout", "Output format.")
 }
 
-func recusive(BYTES_TO_READ uint64, path_to_file string) {
+func recusive(buffer_size uint64, path_to_file string) {
 	fsys := os.DirFS(path_to_file)
 
-	count_file := 0
-	in_queue_file := 0
+	var bar Bar
+
+	var count_file int64 = 0
+	var in_queue_file int64 = 0
 
 	fs.WalkDir(fsys, ".", func(path string, info fs.DirEntry, err error) error {
 		if err != nil {
 			log.Println(err)
 			return err
 		}
-		if info.IsDir() == false {
+		if info.IsDir() == false && path != export_filename {
 			count_file++
 		}
 		return nil
 	})
+	if export_format == "csv" {
+		bar.NewOption(0, count_file)
+	}
 
 	fs.WalkDir(fsys, ".", func(path string, info fs.DirEntry, err error) error {
 		if err != nil {
 			log.Println(err)
 			return err
 		}
-		if info.IsDir() == false {
+		if info.IsDir() == false && path != export_filename {
 			if runtime.GOOS == "windows" {
 				path = strings.ReplaceAll(path, "/", "\\")
 			}
 			uniq_file := path_to_file + path
 			in_queue_file++
-			calc_hash_file(BYTES_TO_READ, uniq_file, in_queue_file, count_file)
+			calc_hash_file(buffer_size, uniq_file, true)
+			if export_format == "csv" {
+				bar.Play(int64(in_queue_file))
+			}
 		}
+
 		return nil
 	})
+	if export_format == "csv" {
+		bar.Finish()
+	}
 
 }
 
-func calc_hash_file(BYTES_TO_READ uint64, uniq_file string, in_queue_file int, count_file int) {
+func calc_hash_file(buffer_size uint64, uniq_file string, is_recursive bool) {
 	fi, err := os.Stat(uniq_file)
 
 	if err != nil {
@@ -144,7 +241,7 @@ func calc_hash_file(BYTES_TO_READ uint64, uniq_file string, in_queue_file int, c
 	// get the size
 	size := fi.Size()
 
-	if size < 2*int64(BYTES_TO_READ) {
+	if size < 2*int64(buffer_size) {
 		// open file
 		file, err := os.Open(uniq_file)
 		if err != nil {
@@ -154,11 +251,11 @@ func calc_hash_file(BYTES_TO_READ uint64, uniq_file string, in_queue_file int, c
 		defer file.Close()
 
 		buf := make([]byte, 30*1024)
-		sha256 := sha256.New()
+		md5 := md5.New()
 		for {
 			n, err := file.Read(buf)
 			if n > 0 {
-				_, err := sha256.Write(buf[:n])
+				_, err := md5.Write(buf[:n])
 				if err != nil {
 					log.Fatal(err)
 				}
@@ -174,8 +271,23 @@ func calc_hash_file(BYTES_TO_READ uint64, uniq_file string, in_queue_file int, c
 			}
 		}
 
-		hash := sha256.Sum(nil)
-		fmt.Printf("%s;%x;%d/%d\n", uniq_file, hash, in_queue_file, count_file)
+		hash := md5.Sum(nil)
+
+		if export_format == "csv" && is_recursive == true {
+			output_csv := fmt.Sprintf("%s;%x;%t\n", uniq_file, hash, false)
+			f, err := os.OpenFile(export_filename, os.O_APPEND|os.O_WRONLY, 0600)
+			if err != nil {
+				panic(err)
+			}
+
+			defer f.Close()
+
+			if _, err = f.WriteString(output_csv); err != nil {
+				panic(err)
+			}
+		} else {
+			fmt.Printf("%s    %x\n", uniq_file, hash)
+		}
 
 	} else {
 		// open file
@@ -188,21 +300,35 @@ func calc_hash_file(BYTES_TO_READ uint64, uniq_file string, in_queue_file int, c
 
 		s := []byte("")
 
-		buffer := make([]byte, BYTES_TO_READ)
+		buffer := make([]byte, buffer_size)
 		first_bytes, err := file.Read(buffer)
 		byte_first_bytes := (buffer[:first_bytes])
 		s = append(s, byte_first_bytes...)
 
-		_, err = file.Seek(int64(-BYTES_TO_READ), io.SeekEnd)
+		_, err = file.Seek(int64(-buffer_size), io.SeekEnd)
 
 		last_bytes, err := file.Read(buffer)
 		byte_last_bytes := (buffer[:last_bytes])
 		s = append(s, byte_last_bytes...)
 
 		//calc hash
-		hash := sha256.Sum256(s)
-		fmt.Printf("%s;%x;%d/%d\n", uniq_file, hash, in_queue_file, count_file)
+		hash := md5.Sum(s)
 
+		if export_format == "csv" && is_recursive == true {
+			output_csv := fmt.Sprintf("%s;%x;%t\n", uniq_file, hash, true)
+			f, err := os.OpenFile(export_filename, os.O_APPEND|os.O_WRONLY, 0600)
+			if err != nil {
+				panic(err)
+			}
+
+			defer f.Close()
+
+			if _, err = f.WriteString(output_csv); err != nil {
+				panic(err)
+			}
+		} else {
+			fmt.Printf("%s    %x\n", uniq_file, hash)
+		}
 	}
 
 }
